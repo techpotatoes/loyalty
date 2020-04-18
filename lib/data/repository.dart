3551daf4528b8ca4bd6e17842de827d5bfbe3b72
Ben Papacek, Lucas Bento and Nick Fan <just-dev+bpapacek+lbento+nfan@tyro.com>
2020-04-18 19:34:27 +1000

    Updating repository

--- lib/data/repository.dart
+++ lib/data/repository.dart
@@ -2,9 +2,9 @@ import 'package:hive/hive.dart';
 
 abstract class Repository<T extends HiveObject> {
   
-  void save(T newObject);
+  Future<void> save(T newObject);
 
-  void delete(T objectToDelete);
+  Future<void> delete(T objectToDelete);
   
   Future<List<T>> getAll();
 }
